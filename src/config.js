9f8029b1bbf8c1aaa0b9c66f193e37eda1173aec
noman122official <67466365+noman122official@users.noreply.github.com>
2021-09-05 11:35:50 +0530

    updated baseurl

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    // baseUrl : "https://havi-backend.herokuapp.com"
-    baseUrl : "http://localhost:9200"
+    baseUrl : "https://havi-backend.herokuapp.com"
+    // baseUrl : "http://localhost:9200"
 }
 
 export default config;
